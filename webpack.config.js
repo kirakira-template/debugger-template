f9d64f5da763384be79b62ca2f14f303d96f689a
wen <nbsawz@gmail.com>
2018-09-21 14:09:31 +0800

    add flow loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,18 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin') 
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const FlowWebpackPlugin = require("flow-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  entry: './debugger/index.js',
-  devtool: 'inline-source-map',
+  mode: "development",
+  entry: "./debugger/index.js",
+  devtool: "inline-source-map",
   output: {
-      path: __dirname + '/dist',
-      filename: 'index_debugger_bundle.js'
+    path: __dirname + "/dist",
+    filename: "index_debugger_bundle.js"
   },
-  plugins: [new HtmlWebpackPlugin({
-      template: './debugger/index.html'
-  })]
-}
+  plugins: [
+    new FlowWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./debugger/index.html"
+    })
+  ]
+};
