95bf93ed2bb51ed983f7fbaa2e5009d2a1509f0f
yuxino <nbsawz@gmail.com>
2020-03-18 16:01:24 +0800

    feat: add file loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,24 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin') 
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  entry: './debugger/index.js',
-  devtool: 'inline-source-map',
+  mode: "development",
+  entry: "./debugger/index.js",
+  devtool: "inline-source-map",
   output: {
-      path: __dirname + '/dist',
-      filename: 'index_debugger_bundle.js'
+    path: __dirname + "/dist",
+    filename: "index_debugger_bundle.js"
   },
-  plugins: [new HtmlWebpackPlugin({
-      template: './debugger/index.html'
-  })]
-}
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./debugger/index.html"
+    })
+  ]
+};
